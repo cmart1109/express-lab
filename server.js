17571d0c8ca24335bab1371e9c992e712c30cdc9
Christian Martinez <christianandremartinez@gmail.com>
2023-09-06 22:46:36 -0600

    express

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const info = [
+    {
+        id: "123456",
+        isCompleted: false,
+        description: "Walk the dog",
+    },
+    {
+        id: "123457",
+        isCompleted: false,
+        description: "Make the dinner",
+    },
+    {
+        id: "134545",
+        isCompleted: true,
+        description: "Clean the Bathroom",
+    },
+    {
+        id: "654322",
+        isCompleted: false,
+        description: "ADA School Homework",
+    },
+    {
+        id: "659142",
+        isCompleted: true,
+        description: "Go to the Grocery Store",
+    }
+];
+
+const jsonData = JSON.stringify(info);
+
+app.get("/", (req, res) => {
+    res.json(info); 
+});
+
+app.listen(port, () => {
+    console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
